1fd8884f05c4f37b289d6f933a7794e06157e4a0
kuldeepparmar246 <22103170@mail.jiit.ac.in>
2024-07-25 15:45:58 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int sum=0;
+
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        sum+=x;
+    }
+
+
+    cout<<(sum*1.0/n)<<endl;
+
+    return 0;
+}
